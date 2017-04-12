596c923713b2ff7121eeffe82c902e6a8db07b2c
kbz <kebingzao@gmail.com>
2017-04-12 10:35:27 +0800

    增加 time 包功能

--- time.go
+++ time.go
@@ -8,18 +8,78 @@ import (
 
 func main() {
 
+	//now := time.Now()
+	//// 时间戳
+	//secs := now.Unix()
+	//nanos := now.UnixNano()
+	//fmt.Println(now)
+	//millis := nanos / 1000000
+	//
+	//fmt.Println(secs)
+	//fmt.Println(millis)
+	//fmt.Println(nanos)
+	//
+	//fmt.Println(time.Unix(secs, 0))
+	//fmt.Println(time.Unix(0, nanos))
+
+	// http://studygolang.com/articles/240
+
+	p := fmt.Println
+
 	now := time.Now()
-	// 时间戳
-	secs := now.Unix()
-	nanos := now.UnixNano()
-	fmt.Println(now)
-	millis := nanos / 1000000
+	p(now)
 
-	fmt.Println(secs)
-	fmt.Println(millis)
-	fmt.Println(nanos)
+	d := time.Duration(7200 * 1000 * 1000 * 1000)
+	p(d)
 
-	fmt.Println(time.Unix(secs, 0))
-	fmt.Println(time.Unix(0, nanos))
+	then := time.Date(
+		2013, 1, 7, 20, 34, 58, 651387237, time.UTC)
+
+	p(then)
+	p(then.Year())
+	p(then.Month())
+	p(then.Day())
+	p(then.Hour())
+	p(then.Minute())
+	p(then.Second())
+	p(then.Nanosecond())
+	p(then.Location())
+	p(then.Weekday())
+
+	p(then.Before(now))
+	p(then.After(now))
+	p(then.Equal(now))
+
+	p(then.Date())
+	p(then.ISOWeek())
+	p("----------")
+	p(now.UTC())
+	p(now.Local())
+	p(now.Location())
+	p(now.Zone())
+	p(now.Unix())
+	p(time.Unix(now.Unix(), 0))
+	p(now.UnixNano())
+	p(time.Unix(0, now.UnixNano()))
+	p(now.GobEncode())
+	p(now.MarshalJSON())
+	p(time.Since(now))
+	p("----------")
+	diff := now.Sub(then)
+	p(diff)
+
+	p(diff.Hours())
+	p(diff.Minutes())
+	p(diff.Seconds())
+	p(diff.Nanoseconds())
+	p(then.Add(diff))
+	p(then.Add(-diff))
+
+	p(d)
+	p(d.Hours())
+	p(d.Minutes())
+	p(d.Seconds())
+	p(d.Nanoseconds())
+	p(then.Add(d))
 
 }
