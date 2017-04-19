d896953faff88c58723fbce7c2040e77b093a470
kbz <kebingzao@gmail.com>
2017-04-19 11:13:07 +0800

    增加 mutex 的示例

---
+++ mutex/example1.go
@@ -0,0 +1,45 @@
+package main
+
+
+
+import (
+	"fmt"
+	"sync"
+	"time"
+)
+
+
+
+var m *sync.Mutex
+
+func main() {
+
+	m = new(sync.Mutex)
+
+	go lockPrint(1)
+
+	lockPrint(2)
+
+	time.Sleep(time.Second)
+
+	fmt.Printf("%s\n", "exit!")
+
+}
+
+
+
+func lockPrint(i int) {
+
+	println(i, "lock start")
+
+	m.Lock()
+
+	println(i, "in lock")
+
+	time.Sleep(3 * time.Second)
+
+	m.Unlock()
+
+	println(i, "unlock")
+
+}
---
+++ pprof/example1.go
@@ -0,0 +1,12 @@
+package main
+
+import (
+	"net/http"
+)
+
+
+func main() {
+	go func() {
+		http.ListenAndServe("localhost:6060", nil)
+	}()
+}
