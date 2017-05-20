2cb3a644b575668fdc3f75b703ec9f34f37d8db8
kbz <kebingzao@gmail.com>
2017-05-20 14:50:33 +0800

    增加随机数实例

--- random-numbers.go
+++ random-numbers.go
@@ -6,9 +6,11 @@ import "math/rand"
 func main() {
 	p := fmt.Println
 	for i:=1 ; i < 100; i++ {
-		p(rand.Intn(100))
+		// 生成 0 <= n < 50 的随机数
+		p(rand.Intn(50))
 	}
 	for i:=1 ; i < 100; i++ {
-		p(rand.Float64() * 10)
+		// 生成 0.0 <= f < 1.0 之间的随机数
+		p(rand.Float64())
 	}
 }
